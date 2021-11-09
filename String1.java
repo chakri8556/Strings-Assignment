af4dce2bf10a097af2f2aed7a72774578058ac77
chakri8556 <chakri8556@gmail.com>
2021-11-09 14:10:35 +0530

    pushing strings assignment to github

---
+++ String1.java
@@ -0,0 +1,8 @@
+ class String1 
+{
+     public static void main(String[] args) 
+    {
+	String s1= new String("Hello");
+        System.out.println(s1.length());
+}
+}
---
+++ String2.java
@@ -0,0 +1,9 @@
+ class String2 {
+     public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = " how are you?";
+		System.out.println(str1.concat(str2));
+                System.out.println(str2.concat(str1));
+                
+	}
+}
